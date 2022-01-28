3f651bc0bf8fc4296efd1704e5f3aed5cccb5a5c
nicoVillagran <nicolasvillagran321@gmail.com>
2022-01-28 01:15:15 -0300

    apartir de este punto voy borrar los comentarios y limpiar el codigo

---
+++ portafolio.js
@@ -0,0 +1,27 @@
+// DOM Elements
+const all = document.querySelector(".all");
+const header = document.querySelector("#header");
+const headerNav = document.querySelector(".header__nav");
+const headerA = document.querySelector(".header__a");
+const stateScroll = scrollY;
+
+// DOM Events
+headerA.addEventListener("click", menuResponsive);
+addEventListener("scroll", windowScroll);
+
+// Associated Functions
+function menuResponsive(){
+    alert("hello, i am a button");
+}
+
+function windowScroll() {
+    if(this.scrollY >= 520 && this.scrollY < 3011) {
+        document.querySelectorAll(".span").forEach(e => e.removeAttribute("hidden"));
+        header.classList.add("header__scroll");
+    }
+    else {
+        header.classList.remove("header__scroll");
+    }
+}
+
+// ------------------------------------------------------------ JS code.
