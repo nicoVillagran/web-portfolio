f15df6aac597b145e8f5d728887c135b049541a1
nicoVillagran <nicolasvillagran321@gmail.com>
2022-01-30 14:26:22 -0300

    se añadieron nuevos svg, y se arreglo el header

--- portafolio.js
+++ portafolio.js
@@ -6,7 +6,7 @@ const headerA = document.querySelector(".header__a");
 const stateScroll = scrollY;
 
 // DOM Events
-headerA.addEventListener("click", menuResponsive);
+// headerA.addEventListener("mouseover", menuResponsive);
 addEventListener("scroll", windowScroll);
 
 // Associated Functions
@@ -15,12 +15,19 @@ function menuResponsive(){
 }
 
 function windowScroll() {
+    let spans = document.querySelectorAll(".nav__span");
+    let newSpan = document.querySelector(".span2");
+
     if(this.scrollY >= 520) {
-        document.querySelectorAll(".span").forEach(e => e.removeAttribute("hidden"));
+        newSpan.classList.replace("header__a", "new__a");
+        // document.querySelectorAll(".span").forEach(e => e.removeAttribute("hidden"));
         header.classList.add("header__scroll");
+        spans.forEach(e => e.style.color = "#1F618D")
     }
     else {
+        newSpan.classList.replace("new__a", "header__a");
         header.classList.remove("header__scroll");
+        spans.forEach(e => e.style.color = "#fff");
     }
 }
 // ------------------------------------------------------------ JS code.
